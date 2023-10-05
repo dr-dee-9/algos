de9bb10cc953e0f5842938f34ebe224cdde05ef9
Deepak Gohil <gohildeepak2003@gmail.com>
2023-10-05 20:23:30 +0530

    fix

--- .bin/install_pip3.sh
+++ .bin/install_pip3.sh
@@ -3,4 +3,4 @@ pip -V
 pip2 -V
 pip3 -V
 sudo -H python3 -m pip3 install --upgrade pip
-sudo -H python3 -m pip3 install --ignore-installed -Ur pip3-requirements.txt
+sudo pip3 install --ignore-installed -Ur pip3-requirements.txt
